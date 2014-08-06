d126b47592596851e8d2e02b91ee07ee1ebbace0
Mike Spensieri <michael.spensieri@radialpoint.com>
2014-08-06 16:07:32 -0400

    updating readme and DemoApp

--- DemoApp/SupportKit.framework/Versions/A/Headers/SupportKit.h
+++ DemoApp/SupportKit.framework/Versions/A/Headers/SupportKit.h
@@ -1,6 +1,6 @@
 //
 //  SupportKit
-//  version : 2.0.1
+//  version : 2.0.2
 //
 //  Copyright (c) 2013 Radialpoint. All rights reserved.
 //
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import "SKTSettings.h"
 
-#define SUPPORTKIT_VERSION @"2.0.1"
+#define SUPPORTKIT_VERSION @"2.0.2"
 
 @interface SupportKit : NSObject
 
