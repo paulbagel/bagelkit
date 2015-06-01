ea271af510a999c6cbc526cad09938ab902d527f
Mike Spensieri <michael.spensieri@radialpoint.com>
2015-06-01 11:47:17 -0400

    updating pod

--- SupportKit.framework/Versions/A/Headers/SupportKit.h
+++ SupportKit.framework/Versions/A/Headers/SupportKit.h
@@ -1,7 +1,7 @@
 //
 //  SupportKit.h
 //  SupportKit
-//  version : 2.7.03
+//  version : 2.7.33
 //
 //  Copyright (c) 2015 Radialpoint. All rights reserved.
 //
@@ -11,7 +11,7 @@
 #import "SKTSettings.h"
 #import "SKTUser.h"
 
-#define SUPPORTKIT_VERSION @"2.7.03"
+#define SUPPORTKIT_VERSION @"2.7.33"
 
 @interface SupportKit : NSObject
 
